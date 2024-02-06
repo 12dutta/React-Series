e15a921a2333305323f90191449c20e158803e37
Vaibhav Dutta <duttabhau12ced@gmail.com>
2024-02-06 12:25:27 +0530

    create through react and Vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React starting | VD</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>React starting with vite | VD</h1>
+  )
+}
+
+export default App
